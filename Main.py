1f7eaa71814649edd7d3f564eb7f02f437c07de1
Catotron <cam29706@gmail.com>
2023-06-21 12:10:35 +1000

    Stop added if the same game is checked twice

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import keno
 from keno import keno_app
 import time
 import datetime
+
+### Terminal Colors
 import os
 os.system("")
 CLEAR = '\33[0m'
@@ -13,7 +15,6 @@ CRED = '\33[91m'
 CYELLOW = '\33[93m'
 CBEIGE = '\33[36m'
 
-
 app = keno_app.KenoAPI("VIC") # choose the state you would like you to get data from
 mode = 1
 
@@ -23,56 +24,58 @@ if mode == 1:
             live_data = app.live_draw()
         except Exception as e:
             print(CRED + str(e))
-
-        ### Numbers Drawn
-        numbers = live_data["draw_numbers"]
-        numbers.sort(key = lambda x: x, reverse = False)
-        draw_numbers = ", ".join(map(str, numbers))
         ### Game Number
         game_number = live_data["game_number"]
-        ### Start Time
-        started_at = live_data["started_at"]
-        start_time = datetime.datetime.strptime(started_at.split('.')[0],'%Y-%m-%d %H:%M:%S') # Removes milliseconds
-        ### Bonus
-        bonus = live_data["bonus"]
-        if bonus == "reg":
-            multiplier = 1
-            bonus = CRED + "reg"
+        if last_game_number == game_number: # in case the same game is called twice, Skip
+            time.sleep(20)
+        else:
+            last_game_number = game_number
+            ### Numbers Drawn
+            numbers = live_data["draw_numbers"]
+            numbers.sort(key = lambda x: x, reverse = False)
+            draw_numbers = ", ".join(map(str, numbers))
+            ### Start Time
+            started_at = live_data["started_at"]
+            start_time = datetime.datetime.strptime(started_at.split('.')[0],'%Y-%m-%d %H:%M:%S') # Removes milliseconds
+            ### Bonus
+            bonus = live_data["bonus"]
+            if bonus == "reg":
+                multiplier = 1
+                bonus = CRED + "reg"
 
-        elif bonus == "x2":
-            multiplier = 2
-            bonus = CYELLOW + "x2"
+            elif bonus == "x2":
+                multiplier = 2
+                bonus = CYELLOW + "x2"
 
-        elif bonus == "x3":
-            multiplier = 3
-            bonus = CGREEN + "x3"
+            elif bonus == "x3":
+                multiplier = 3
+                bonus = CGREEN + "x3"
 
-        elif bonus == "x4":
-            multiplier = 4
-            bonus = CGREENHIGHLIGHT + "x4"
-        
-        else: print(CRED + "Error unknown bonus:" + str(bonus)) # incase there is any higher multiplier
+            elif bonus == "x4":
+                multiplier = 4
+                bonus = CGREENHIGHLIGHT + "x4"
+            
+            else: print(CRED + "Error unknown bonus:" + str(bonus)) # incase there is any higher multiplier
 
-        ### Heads/Tails
-        HTresult = live_data["result"]
-        if HTresult == "tails": HTresult = CBLUE + "Tails"
-        elif HTresult == "heads": HTresult = CRED + "Heads"
-        elif HTresult == "evens": HTresult = CBEIGE + "Evens"
-        Hresult = live_data["heads"]
-        Tresult = live_data["tails"]
+            ### Heads/Tails
+            HTresult = live_data["result"]
+            if HTresult == "tails": HTresult = CBLUE + "Tails"
+            elif HTresult == "heads": HTresult = CRED + "Heads"
+            elif HTresult == "evens": HTresult = CBEIGE + "Evens"
+            Hresult = live_data["heads"]
+            Tresult = live_data["tails"]
 
-        ### Time
-        current_time = datetime.datetime.utcnow()
-        current_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
-        ### Build Terminal
-        print(CBLUE + "Keno Tracker" + CLEAR)
-        print("Game Number: " + str(game_number) + "  |  Game Started at: " + str(start_time) + " UTC  |  Data Pulled at: " + str(current_time) + " UTC")
-        print("Numbers Drawn: " + CGREEN + draw_numbers + CLEAR)
-        print("Multiplier: " + str(bonus) + CLEAR)
-        print("Heads/Tails Result: " + str(HTresult) + CLEAR + "  |  " + CRED + "Heads: " + str(Hresult) + CBLUE + "  Tails: " + str(Tresult) + CLEAR)
-        print(CBLUE + "---------------------------------------------------------------------")
-
-        time.sleep(180) # Adjust to be closer to start time
+            ### Time
+            current_time = datetime.datetime.utcnow()
+            current_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
+            ### Build Terminal
+            print(CBLUE + "Keno Tracker" + CLEAR)
+            print("Game Number: " + str(game_number) + "  |  Game Started at: " + str(start_time) + " UTC  |  Data Pulled at: " + str(current_time) + " UTC")
+            print("Numbers Drawn: " + CGREEN + draw_numbers + CLEAR)
+            print("Multiplier: " + str(bonus) + CLEAR)
+            print("Heads/Tails Result: " + str(HTresult) + CLEAR + "  |  " + CRED + "Heads: " + str(Hresult) + CBLUE + "  Tails: " + str(Tresult) + CLEAR)
+            print(CBLUE + "---------------------------------------------------------------------")
+            time.sleep(180) # Adjust to be closer to start time
 
 elif mode == 2: # Hot/Cold
     trending_numbers = app.hot_cold()
